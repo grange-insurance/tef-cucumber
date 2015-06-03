9909d0d4e38787e396d5306c8702e58f4abe4dca
Eric Kessler <morrow748@gmail.com>
2015-06-03 10:35:28 -0400

    More relative code path removal.
    Replaced the relative path require statements for common rake tasks
    with ones that use the development gem instead.

--- rakefiles/deployment_tasks.rb
+++ rakefiles/deployment_tasks.rb
@@ -1,5 +1,5 @@
 require_relative 'common_tasks'
-require_relative '../../tef/rakefiles/deployment_tasks'
+require 'tef/development/deployment_tasks'
 
 
 namespace 'tef' do
@@ -15,19 +15,19 @@ namespace 'tef' do
     end
 
     desc 'Spin up a local version of a queuebert'
-    task :create_queuebert => [:set_environment] do
+    task :create_queuebert => [:set_tef_environment] do
       # todo - Assuming Windows OS for the moment
       Process.spawn('start "Queuebert" cmd /c bundle exec start_tef_queuebert')
     end
 
     desc 'Spin up a local version of a cuke worker'
-    task :create_cuke_worker => [:set_environment] do
+    task :create_cuke_worker => [:set_tef_environment] do
       # todo - Assuming Windows OS for the moment
       Process.spawn('start "CukeWorker" cmd /c bundle exec start_tef_cuke_worker')
     end
 
     desc 'Spin up a local version of a cucumber keeper'
-    task :create_cuke_keeper => [:set_environment] do
+    task :create_cuke_keeper => [:set_tef_environment] do
       # todo - Assuming Windows OS for the moment
       Process.spawn('start "CucumberKeeper" cmd /c bundle exec start_tef_cuke_keeper')
     end
