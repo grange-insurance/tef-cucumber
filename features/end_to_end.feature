80e333e6936f9f2b40040b4f8a831e61cba3c1ce
Eric Kessler <morrow748@gmail.com>
2015-06-16 20:27:56 -0400

    More worker nodes used for testing.
    Increased the number of worker nodes created during high level
    testing, both to increase the speed at which the tests can finish and
    to create a more realistic use case.

--- features/end_to_end.feature
+++ features/end_to_end.feature
@@ -9,7 +9,7 @@ Feature: End to end acceptance testing
   Scenario: Cucumber task
     Given a queuebert node is running
     And a manager node is running
-    And a cuke worker node is running
+    And cuke worker nodes are running
     And a cuke keeper node is running
     When  a request for a test suite is sent
     Then results for the executed tests are stored by the keeper
--- features/step_definitions/setup_steps.rb
+++ features/step_definitions/setup_steps.rb
@@ -11,6 +11,16 @@ And(/^a cuke worker node is running$/) do
   Process.detach(@cuke_worker_pid)
 end
 
+And(/^(?:"([^"]*)" )?cuke worker nodes are running$/) do |worker_count|
+  @cuke_worker_pids ||= []
+  worker_count = worker_count ? worker_count.to_i : 5
+
+  worker_count.times do
+    @cuke_worker_pids << Process.spawn('start "Cuke Worker" cmd /c bundle exec start_tef_cuke_worker')
+    Process.detach(@cuke_worker_pids.last)
+  end
+end
+
 And(/^a cuke keeper node is running$/) do
   @cuke_keeper_pid = Process.spawn('start "Cuke Keeper" cmd /c bundle exec start_tef_cuke_keeper')
   Process.detach(@cuke_keeper_pid)
