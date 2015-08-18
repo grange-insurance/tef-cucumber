f23b396f8ee74f2e6000d24212cecc6cc70eb6cc
Eric Kessler <morrow748@gmail.com>
2015-08-18 14:01:52 -0400

    Removing extraneous files.
    Removed some old binary files that are not needed since they are just
    wrappers around the intended binary file and such wrappers are
    created automatically when the gems are installed if they are needed
    for a particular OS.

--- gems/tef-cuke_keeper/bin/start_cuke_keeper.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title TEF Cuke Keeper
-if NOT DEFINED BUNDLE_MODE set BUNDLE_MODE=dev
-if NOT DEFINED TEF_AMQP_URL_DEV set TEF_AMQP_URL_DEV=amqp://localhost:5672
-call bundle exec ruby bin\start_tef_cuke_keeper
